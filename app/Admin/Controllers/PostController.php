dcefe0f0a459ee148845afe62d199386e026673a
tomonari <tomonari@forefrontservice.co.jp>
2023-10-27 12:01:41 +0900

    php artisan admin:make PostController --model=App\\Post composer require "doctrine/dbal:2.*" 二つのコマンドを実行

---
+++ app/Admin/Controllers/PostController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Post;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class PostController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Post';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Post());
+
+        $grid->column('id', __('Id'));
+        $grid->column('user_id', __('User id'));
+        $grid->column('content', __('Content'));
+        $grid->column('image_path', __('Image path'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Post::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('user_id', __('User id'));
+        $show->field('content', __('Content'));
+        $show->field('image_path', __('Image path'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Post());
+
+        $form->number('user_id', __('User id'));
+        $form->textarea('content', __('Content'));
+        $form->text('image_path', __('Image path'));
+
+        return $form;
+    }
+}
